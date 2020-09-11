815ffe61622b38243de5fa9d269787c0a8231659
rhea-manuel <rheamanuel02@gmail.com>
2020-09-11 11:58:01 -0400

    Completed one character

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character'
+
+import Axios from 'axios'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [allCharacters, changeCharacters] = useState([])
+
+  useEffect(() => {
+    Axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+      .then(response => {
+        console.log(response.data)
+        changeCharacters(response.data)
+      })
+      .catch(error => console.log(error))
+  }, [])
+
+  // const frontView = allCharacters['sprites']['front_default']
+  // console.log(allCharacters)
+
+  const curCharacter = allCharacters
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character sprites={curCharacter.sprites} name={curCharacter.name} abilities={curCharacter.abilities} stats={curCharacter.stats}></Character>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,34 @@
 // Write your Character component here
+import React, {useState, useEffect} from 'react';
+
+export default function Character ({sprites, name, abilities, stats}){
+
+    if (!sprites){
+        return (
+            <div>loading</div>
+        )
+    }
+
+    // console.log(props.sprites.front_default)
+    // const frontView = `"${props.sprites.front_default}"`
+
+    return (
+        <div>
+            <img src = {sprites.front_default}></img>
+            <h2>{name}</h2>
+            {
+                abilities.map(item => {
+                return <div key={item.slot}>Ability: {item.ability.name}, Slot: {item.slot}</div>
+                })
+            }
+
+            {
+                stats.map((item,index) => {
+                    return <div key = {index}>
+                        <a href={item.stat.url}>{item.stat.name}: </a>
+                        {item.base_stat}</div>
+                })
+            }
+        </div>
+    )
+}
